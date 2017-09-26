387c96503b5c7be9bcf046934722a119e16c5436
Ben Williamson <mr.ben.williamson@gmail.com>
2017-09-26 18:22:42 +0100

    Created and synced everything
    (i hope)

---
+++ StishBoard/StishBoard/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StishBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Turn and face the strange!
+        }
+    }
+}
