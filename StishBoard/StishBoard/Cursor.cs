73d6b0f354718b976590ace136031a875ab7f7ec
TheRealBenWilliamson <mr.ben.williamson@gmail.com>
2018-10-19 15:24:06 +0100

    GameMaster class
    with a few bugs here and there

--- StishBoard/StishBoard/Cursor.cs
+++ StishBoard/StishBoard/Cursor.cs
@@ -51,8 +51,6 @@ namespace StishBoard
         
         public enum Mode { free, locked };
         private Mode mode = Mode.free;
-        public enum Purchase { Barracks, Unit, Burst };
-        private Purchase purchase;
 
         public Mode CursorMode
         {
@@ -181,260 +179,10 @@ namespace StishBoard
             Console.ResetColor();
             Console.SetCursorPosition(0, 13);
 
-        }
-
-        private void Drag(Coordinate FromCo, Coordinate ToCo, Player MyPlayer)
-        {
-            Square From = board.getSquare(FromCo);
-            Square To = board.getSquare(ToCo);
-
-            From.Owner = MyPlayer;
-            To.Owner = MyPlayer;
-
-            To.Dep = From.Dep;
-            From.Dep = new Empty();
-        }
-
-
-        /*
-        private void SearchAndPlace(uint valueX, uint valueY, List<uint> SlistX, List<uint> SlistY, List<uint> PlistX, List<uint> PlistY)
-        {
-            bool found = false;
-            for (int index = 0; index < SlistX.Count; index++)
-            {
-                if ((SlistX[index] == valueX) && (SlistY[index] == valueY))
-                {
-                    found = true;
-                }
-            }
-            if (found == false)
-            {
-                PlistX.Add(valueX);
-                PlistY.Add(valueY);
-            }
-        }
-
-        private bool Search(uint valueX, uint valueY, List<uint> SlistX, List<uint> SlistY)
-        {
-            bool found = false;
-            for (int index = 0; index < SlistX.Count; index++)
-            {
-                if ((SlistX[index] == valueX) && (SlistY[index] == valueY))
-                {
-                    found = true;
-                }
-            }
-            return found;
-        }
+        }       
 
         
-        public void TerritoryDeaths()
-        {
-            for (int look = 0; look < 2; look++)
-            {
-                Player LookPlayer;
-                //lists as we dont want a limit that would be given by an array
-                List<uint> ToCheckX = new List<uint>();
-                List<uint> ToCheckY = new List<uint>();
-                List<uint> CheckedX = new List<uint>();
-                List<uint> CheckedY = new List<uint>();
-                uint investX = 5;
-                uint investY;
-                
-                if(look == 0)
-                {
-                    LookPlayer = board.Player1;
-                    investY = 9;
-                }
-                else
-                {
-                    LookPlayer = board.Player2;
-                    investY = 1;
-                }
-               
-                ToCheckX.Add(investX);
-                ToCheckY.Add(investY);
-                //Checking Invest
-                while(ToCheckX.Count != 0)
-                {
-                    investX = ToCheckX[0];
-                    investY = ToCheckY[0];
-
-                    ToCheckX.Remove(investX);
-                    ToCheckY.Remove(investY);
-                    CheckedX.Add(investX);
-                    CheckedY.Add(investY);
-                    if (board.getSquare((investX - 1), investY) != null)
-                    {
-                        //left
-                        if (board.getSquare((investX - 1), investY).Owner == LookPlayer)
-                        {
-                            SearchAndPlace(investX - 1, investY, CheckedX, CheckedY, ToCheckX, ToCheckY);
-                        }
-                    }
-                    if (board.getSquare(investX, (investY - 1)) != null)
-                    {
-                        //up
-                        if (board.getSquare(investX, (investY - 1)).Owner == LookPlayer)
-                        {
-                            SearchAndPlace(investX, investY - 1, CheckedX, CheckedY, ToCheckX, ToCheckY);
-                        }
-                    }
-                    if (board.getSquare((investX + 1), investY) != null)
-                    {
-                        //right
-                        if (board.getSquare((investX + 1), investY).Owner == LookPlayer)
-                        {
-                            SearchAndPlace(investX + 1, investY, CheckedX, CheckedY, ToCheckX, ToCheckY);
-                        }
-                    }
-                    if (board.getSquare(investX, (investY + 1)) != null)
-                    {
-                        //down
-                        if (board.getSquare(investX, (investY + 1)).Owner == LookPlayer)
-                        {
-                            SearchAndPlace(investX, investY + 1, CheckedX, CheckedY, ToCheckX, ToCheckY);
-                        }
-                    }                                                       
-                }
-
-                for (uint y = 0; y < board.BoardSize; y++)
-                {
-                    for (uint x = 0; x < board.BoardSize; x++)
-                    {
-                        if ((board.getSquare(x,y).Owner == LookPlayer) && (Search(x,y,CheckedX,CheckedY) == false))
-                        {
-                            if(board.getSquare(x,y).Dep.DepType == "Barracks")
-                            {
-                                board.getSquare(x, y).Dep.OwnedBy = null;
-                                board.getSquare(x, y).Dep.Health = 0;
-                            }
-                            else
-                            {
-                                board.getSquare(x, y).Dep = new Empty();
-                            }
-                            board.getSquare(x, y).Owner = null;
-                        }
-                        
-                    }
-                }
-
-
-            }
-        }
         
-
-        public void BaseSquares()
-        {
-            for (int look = 0; look < 2; look++)
-            {
-                Player LookPlayer;
-                uint min = 0, max = 0;
-                if (look == 0)
-                {
-                    LookPlayer = board.Player1;
-                    min = board.Player1.BaseY - 1;
-                    max = board.Player1.BaseY + 1;
-                }
-                else
-                {
-                    LookPlayer = board.Player2;
-                    min = board.Player2.BaseY - 1;
-                    max = board.Player2.BaseY + 1;
-                }
-              
-
-                for (uint y = min; y < max; y++)
-                {
-                    for (uint x = (board.BoardSize/2) - 1; x < (board.BoardSize / 2) + 1; x++)
-                    {
-                        if (board.getSquare(x, y).Owner == null)
-                        {
-                            board.getSquare(x, y).Owner = LookPlayer;
-                        }
-                    }
-                }
-
-            }
-        }
-        */
-        
-        public bool Action(Coordinate From, Coordinate Check , Player MyPlayer)
-        {
-            //the bool output lets the caller know if the unit moved
-            bool Moved = false;
-            String CheckDep = board.getSquare(Check).Dep.DepType;
-            Player Owner = board.getSquare(Check).Dep.OwnedBy;
-            if (((CheckDep == "Empty") || (CheckDep == "Barracks") && Owner == MyPlayer))
-            {
-                //drag or destroys a friendly barracks
-                if(board.getSquare(From).Dep.MP > 0)
-                {
-                    board.getSquare(From).Dep.MP--;
-                    Drag(new Coordinate(Pos.X,Pos.Y), new Coordinate(Check.X,Check.Y), MyPlayer);
-                    Moved = true;
-                }
-                
-            }
-            else if ((CheckDep == "Unit" || CheckDep == "Barracks" || CheckDep == "Base") && (Owner != MyPlayer))
-            {
-                //attack
-                //adjust health and then if the attacking unit won, use the drag function
-                //i dont know if i want to use the drag function on an attack. i will wait until i test it to decide
-                //i dont have to redefine these squares but i think it helps the code read better
-                Square Attacker = board.getSquare(From);
-                Square Defender = board.getSquare(Check);
-                
-                //attacker must have more some MP to attack to prevent spawn attacking
-                if (Attacker.Dep.MP > 0)
-                {
-                    //checks who wins the combat: attacker or defender
-                    //attacker wins
-                    if (Attacker.Dep.Health > Defender.Dep.Health)
-                    {
-                        Attacker.Dep.Health -= Defender.Dep.Health;
-                        //Barracks are a special case
-                        if (CheckDep == "Barracks")
-                        {
-                            board.getSquare(Check).Dep = new Barracks(MyPlayer, board.getSquare(Check), 5);
-                            board.getSquare(Check).Owner = MyPlayer;
-                        }
-                        else
-                        {
-                            Defender.Dep = new Empty();
-                        }
-
-                    }
-                    //defender wins
-                    else if (Attacker.Dep.Health < Defender.Dep.Health)
-                    {
-                        Defender.Dep.Health -= Attacker.Dep.Health;
-                        Attacker.Dep = new Empty();
-                        mode = Mode.free;
-                    }
-                    //both have equal health
-                    else
-                    {
-                        if (CheckDep == "Barracks")
-                        {
-                            board.getSquare(Check).Dep = new Barracks(MyPlayer, board.getSquare(Check), 5);
-                            board.getSquare(Check).Owner = MyPlayer;
-                        }
-                        else
-                        {
-                            Defender.Dep = new Empty();
-                        }
-                        Attacker.Dep = new Empty();
-                        mode = Mode.free;
-                    }
-                }
-            }
-
-            //TerritoryDeaths();
-            //BaseSquares();
-            return Moved;
-        }
-
         public bool Land(Coordinate Check, Player MyPlayer)
         {
             if (board.getSquare(Check).Dep.DepType == "Unit" && board.getSquare(Check).Dep.OwnedBy == MyPlayer)
@@ -444,49 +192,7 @@ namespace StishBoard
             else return false;
         }       
 
-        private void BuyDep(Coordinate Pur,Player ConPlayer)
-        {
-            if((board.getSquare(Pur) != null) && (board.getSquare(Pur).Dep.DepType == "Empty") && (board.getSquare(Pur).Owner == ConPlayer))
-            {
-                //purchase is acceptable
-                if (purchase == Purchase.Barracks)
-                {
-                    //check how many barracks the player already has and multiply it buy the cost of one barracks
-                    uint multiply = 0;
-                    Coordinate See = new Coordinate();
-
-                    for (uint y = 0; y < board.BoardSize; y++)
-                    {
-                        for (uint x = 0; x < board.BoardSize; x++)
-                        {
-                            See.X = x;
-                            See.Y = y;
-                            if ((board.getSquare(See).Dep.DepType == "Barracks" || board.getSquare(See).Dep.DepType == "Base") && board.getSquare(See).Dep.OwnedBy == ConPlayer)
-                            {
-                                multiply++;
-                            }
-                        }
-                    }
-
-                    if(ConPlayer.Balance >= 3 * multiply)
-                    {
-                        board.getSquare(Pur).Dep = new Barracks(board.getSquare(Pur).Owner, board.getSquare(Pur), 5);
-                        ConPlayer.Balance -= 3 * multiply;
-                    }
-                    
-                }
-                else if(purchase == Purchase.Unit)
-                {
-                    //spend the entire player balance 
-                    if(ConPlayer.Balance > 0)
-                    {
-                        board.getSquare(Pur).Dep = new Unit(board.getSquare(Pur).Owner, board.getSquare(Pur), ConPlayer.Balance);
-                        ConPlayer.Balance = 0;
-                    }
-                    
-                }               
-            }
-        }
+        
 
         public void Move(Player ConPlayer, string input)
         {
@@ -532,14 +238,12 @@ namespace StishBoard
             else if (input == "Q")
             {
                 //buy barracks
-                purchase = Purchase.Barracks;
-                BuyDep(CursorCoord, ConPlayer);
+                master.BuyBarracks(CursorCoord, ConPlayer);
             }
             else if (input == "E")
             {
                 //buy unit
-                purchase = Purchase.Unit;
-                BuyDep(CursorCoord, ConPlayer);
+                master.BuyUnit(CursorCoord, ConPlayer);
             }
             else if (input == "_")
             {
@@ -558,7 +262,7 @@ namespace StishBoard
                 //locked
                 if (CursorMode == Mode.locked)
                 {
-                    if (Action(Pos, CursorCoord, ConPlayer) == true)
+                    if (master.Action(Pos, CursorCoord, ConPlayer) == true)
                     {
                         Pos.X = CursorCoord.X;
                         Pos.Y = CursorCoord.Y;
--- StishBoard/StishBoard/GameMaster.cs
+++ StishBoard/StishBoard/GameMaster.cs
@@ -25,6 +25,7 @@ namespace StishBoard
         }
 
         StishBoard board = StishBoard.Instance;
+        Cursor cursor = Cursor.Instance;
 
         //coordinates are uints so anything less than 0 will overflow
         public bool OnBoard(Coordinate num)
@@ -47,6 +48,144 @@ namespace StishBoard
                 return false;
             }
         }
+
+        public void BuyBarracks(Coordinate Pur, Player ConPlayer)
+        {
+            if ((board.getSquare(Pur) != null) && (board.getSquare(Pur).Dep.DepType == "Empty") && (board.getSquare(Pur).Owner == ConPlayer))
+            {
+                //check how many barracks the player already has and multiply it buy the cost of one barracks
+                uint multiply = 0;
+                Coordinate See = new Coordinate();
+
+                for (uint y = 0; y < board.BoardSize; y++)
+                {
+                    for (uint x = 0; x < board.BoardSize; x++)
+                    {
+                        See.X = x;
+                        See.Y = y;
+                        if ((board.getSquare(See).Dep.DepType == "Barracks" || board.getSquare(See).Dep.DepType == "Base") && board.getSquare(See).Dep.OwnedBy == ConPlayer)
+                        {
+                            multiply++;
+                        }
+                    }
+                }
+
+                if (ConPlayer.Balance >= 3 * multiply)
+                {
+                    board.getSquare(Pur).Dep = new Barracks(board.getSquare(Pur).Owner, board.getSquare(Pur), 5);
+                    ConPlayer.Balance -= 3 * multiply;
+                }
+            }
+        }
+
+        public void BuyUnit(Coordinate Pur, Player ConPlayer)
+        {
+            if ((board.getSquare(Pur) != null) && (board.getSquare(Pur).Dep.DepType == "Empty") && (board.getSquare(Pur).Owner == ConPlayer))                
+            {
+                //spend the entire player balance 
+                if (ConPlayer.Balance > 0)
+                {
+                    board.getSquare(Pur).Dep = new Unit(board.getSquare(Pur).Owner, board.getSquare(Pur), ConPlayer.Balance);
+                    ConPlayer.Balance = 0;
+                }                 
+            }
+        }
+
+        private void Drag(Coordinate FromCo, Coordinate ToCo, Player MyPlayer)
+        {
+            Square From = board.getSquare(FromCo);
+            Square To = board.getSquare(ToCo);
+
+            From.Owner = MyPlayer;
+            To.Owner = MyPlayer;
+
+            To.Dep = From.Dep;
+            From.Dep = new Empty();
+        }
+
+        public void Attack(Coordinate From, Coordinate Check, Player MyPlayer)
+        {
+            //attack
+            //adjust health and then if the attacking unit won, use the drag function
+            //i dont know if i want to use the drag function on an attack. i will wait until i test it to decide
+            //i dont have to redefine these squares but i think it helps the code read better
+            Square Attacker = board.getSquare(From);
+            Square Defender = board.getSquare(Check);
+            String CheckDep = board.getSquare(Check).Dep.DepType;
+            Player Owner = board.getSquare(Check).Dep.OwnedBy;
+
+            //attacker must have more some MP to attack to prevent spawn attacking
+            if (Attacker.Dep.MP > 0)
+            {
+                //checks who wins the combat: attacker or defender
+                //attacker wins
+                if (Attacker.Dep.Health > Defender.Dep.Health)
+                {
+                    Attacker.Dep.Health -= Defender.Dep.Health;
+                    //Barracks are a special case
+                    if (CheckDep == "Barracks")
+                    {
+                        board.getSquare(Check).Dep = new Barracks(MyPlayer, board.getSquare(Check), 5);
+                        board.getSquare(Check).Owner = MyPlayer;
+                    }
+                    else
+                    {
+                        Defender.Dep = new Empty();
+                    }
+
+                }
+                //defender wins
+                else if (Attacker.Dep.Health < Defender.Dep.Health)
+                {
+                    Defender.Dep.Health -= Attacker.Dep.Health;
+                    Attacker.Dep = new Empty();
+                    cursor.CursorMode = Cursor.Mode.free;
+                }
+                //both have equal health
+                else
+                {
+                    if (CheckDep == "Barracks")
+                    {
+                        board.getSquare(Check).Dep = new Barracks(MyPlayer, board.getSquare(Check), 5);
+                        board.getSquare(Check).Owner = MyPlayer;
+                    }
+                    else
+                    {
+                        Defender.Dep = new Empty();
+                    }
+                    Attacker.Dep = new Empty();
+                    cursor.CursorMode = Cursor.Mode.free;
+                }
+            }
+        }
+
+        public bool Action(Coordinate From, Coordinate Check, Player MyPlayer)
+        {
+            //the bool output lets the caller know if the unit moved
+            bool Moved = false;
+            String CheckDep = board.getSquare(Check).Dep.DepType;
+            Player Owner = board.getSquare(Check).Dep.OwnedBy;
+            if (((CheckDep == "Empty") || (CheckDep == "Barracks") && Owner == MyPlayer))
+            {
+                //drag or destroys a friendly barracks
+                if (board.getSquare(From).Dep.MP > 0)
+                {
+                    board.getSquare(From).Dep.MP--;
+                    Drag(new Coordinate(From.X, From.Y), new Coordinate(Check.X, Check.Y), MyPlayer);
+                    Moved = true;
+                }
+
+            }
+            else if ((CheckDep == "Unit" || CheckDep == "Barracks" || CheckDep == "Base") && (Owner != MyPlayer))
+            {
+                Attack(From, Check, MyPlayer);
+            }
+
+            //TerritoryDeaths();
+            //BaseSquares();
+            return Moved;
+        }
+
 
     }
 }
