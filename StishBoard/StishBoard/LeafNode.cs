aa1f1d3d57e560d6ad6ca8429950d5f89a5ac349
TheRealBenWilliamson <mr.ben.williamson@gmail.com>
2018-11-10 21:58:52 +0000

    Nodes Nodes Nodes
    Added a bunch of Node Classes and outlines their purpose

---
+++ StishBoard/StishBoard/LeafNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StishBoard
+{
+    class LeafNode : ValuedTreeNode
+    {
+        LeafNode(TreeNode Parent) : base(Parent)
+        {
+
+        }
+
+        public void Evaluate()
+        {
+            //should be called on a leaf node (neither alpha nor beta but has a value)
+            //calls the PathNode function to find a list with the reverse chronological order of turns taken from the present to this node.
+            //assigns a value to this node that represents how good this sequence of moves is for this player (AI)
+            List<TreeNode> MyPath = new List<TreeNode>();
+            MyPath = this.PathNodes();
+        }
+    }
+}
---
+++ StishBoard/StishBoard/MinNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StishBoard
+{
+    class MinNode : ValuedTreeNode
+    {
+        MinNode(TreeNode Parent) : base(Parent)
+        {
+
+        }
+
+        public void CheckChildren()
+        {
+
+        }
+    }
+}
