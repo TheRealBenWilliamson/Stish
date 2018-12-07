e2d825b6f94d6cd1fb4731c6dd33d0fd8f3402a8
TheRealBenWilliamson <mr.ben.williamson@gmail.com>
2018-12-07 13:27:32 +0000

    Begginning path finding

--- StishBoard/StishBoard/ForeSight.cs
+++ StishBoard/StishBoard/ForeSight.cs
@@ -12,7 +12,73 @@ namespace StishBoard
         //these functions will be called by the node objects in order to tell the nodes, how to configure their children
 
         //predict will take a boardstate argument and then create a new board state and node for each possible future move
-        
+
+        public bool Connected(Coordinate MoveFrom, Coordinate MoveTo)
+        {
+            //go from "MoveTo" to "MoveFrom"
+            //use a similar path finding algorithm to the territory bursts but incentivise a direction.
+            //searching a new node costs a movement point. branch termminates when all points are used. tree terminates when all branches have concluded
+
+            bool Connect = true;
+            //lists as we dont want a limit that would be given by an array
+            List<Coordinate> ToCheck = new List<Coordinate>();
+            List<Coordinate> Checked = new List<Coordinate>();
+            List<Coordinate> Path = new List<Coordinate>();
+            Coordinate Search = new Coordinate();
+
+            //TO DO search cardinals in order of distance. use CASES!
+
+            ToCheck.Add(MoveTo);
+            while(ToCheck.Count != 0)
+            {
+                Search = ToCheck[0];
+                ToCheck.Remove(Search);
+                Checked.Add(Search);
+
+                Coordinate Up = new Coordinate(Search.X,Search.Y - 1);
+                Coordinate Right = new Coordinate(Search.X + 1, Search.Y);
+                Coordinate Down = new Coordinate(Search.X, Search.Y + 1);
+                Coordinate Left = new Coordinate(Search.X - 1, Search.Y);
+                
+
+                if ((StishBoard.Instance.getSquare(Up) != null) && (StishBoard.Instance.getSquare(Up).Dep.DepType == "Empty"))
+                {
+                    ToCheck.Add(Up);
+                }
+                if ((StishBoard.Instance.getSquare(Right) != null) && (StishBoard.Instance.getSquare(Right).Dep.DepType == "Empty"))
+                {
+                    ToCheck.Add(Right);
+                }
+                if ((StishBoard.Instance.getSquare(Down) != null) && (StishBoard.Instance.getSquare(Down).Dep.DepType == "Empty"))
+                {
+                    ToCheck.Add(Down);
+                }
+                if ((StishBoard.Instance.getSquare(Left) != null) && (StishBoard.Instance.getSquare(Left).Dep.DepType == "Empty"))
+                {
+                    ToCheck.Add(Left);
+                }
+            }
+
+
+
+            return Connect;
+        }
+
+
+        public bool Route(Coordinate From, Coordinate To)
+        {
+            //the temp and final lists are used to record which path was used to get to the unit.
+            //this route function will make sure there are no obstructions and an appropriate territory path will follow an AIs unit as it moves.
+            List<Coordinate> Temp = new List<Coordinate>();
+            List<Coordinate> Final = new List<Coordinate>();
+            bool Obstruct = false;
+            Coordinate look = new Coordinate();
+
+            //there is often more than one route to a position but the fewest moves will be a higher priority
+             
+            return Obstruct;
+        }
+
         //this may not need to be void however i dont yet know if or what the output will be (currently just counts)
         public void Sweep(Coordinate Check, Player Side)
         {
--- StishBoard/StishBoard/Program.cs
+++ StishBoard/StishBoard/Program.cs
@@ -46,9 +46,10 @@ namespace StishBoard
 
             //TEST PURPOSES
             ForeSight test = new ForeSight();
-            Coordinate testCo = new Coordinate(4, 5);
-            
-            
+            Coordinate testCo = new Coordinate(5, 5);
+            Coordinate testCo2 = new Coordinate(3, 5);
+
+
 
             //game loop takes place here
             bool GameEnd = false;
